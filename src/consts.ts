18c21f817d6d457b2a2aef914526cc9c1564dd4b
ffy6511 <git@stackblitz.com>
2024-12-18 20:29:59 +0800

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '待抉的博客';
+export const SITE_DESCRIPTION = '欢迎来到我的个人网站！';
+export const AUTHOR_NAME = '待抉';
+export const SOCIAL_LINKS = {
+  github: 'https://github.com/withastro/astro',
+  twitter: 'https://twitter.com/astrodotbuild',
+  mastodon: 'https://m.webtoo.ls/@astro'
+} as const;
---
+++ src/content/blog/test.md
@@ -0,0 +1,9 @@
+---
+title: 'test'
+description: 'Lorem ipsum dolor sit amet'
+pubDate: 'Jul 17 2022'
+heroImage: '/blog-placeholder-4.jpg'
+---
+
+## This is just a test page
+aaa
