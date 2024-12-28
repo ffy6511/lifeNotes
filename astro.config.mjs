bad38bd4413de39da5cd635576b709c86b809a5d
ffy6511 <1253876012@qq.com>
2024-12-28 18:57:04 +0800

    Revert "docs: build for pages"
    This reverts commit d7374b6951400f1478e05b33b7fc1e673b7d6871.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,4 @@ import preact from '@astrojs/preact';
 export default defineConfig({
     site: 'https://example.com',
     integrations: [mdx(), sitemap(), preact()],
-    output: 'static', // 生成静态文件
-    base: '/',        // 基础路径
-    build: {
-        outDir: './dist', // 指定输出路径
-    },
 });
